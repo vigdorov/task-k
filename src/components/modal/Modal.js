52c5d8f73b18375105aaa07937c63fd55c83a086
vigdorov <vigdorov@yandex-team.ru>
2020-04-05 12:42:35 +0300

    ьщвфд

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,23 @@
+const modalWindow = {
+    mainDiv: document.getElementById('modalWindow'),
+    closeButton: document.getElementById('modalCloseButton'),
+    saveButton: document.getElementById('modalSaveButton'),
+    isOpen: function() {
+        return this.mainDiv.style.display === 'flex';
+    },
+    showWindow: function() {
+        this.mainDiv.style.display = 'flex';
+    },
+    closeWindow: function() {
+        this.mainDiv.style.display = 'none';
+    }
+};
+
+modalWindow.mainDiv.addEventListener('click', (event) => {
+    const isMainDiv = event.target.getAttribute('id') === 'modalWindow';
+    if (modalWindow.isOpen() && isMainDiv) {
+        modalWindow.closeWindow();
+    }
+});
+
+modalWindow.closeButton.addEventListener('click', () => modalWindow.closeWindow());
--- src/script.js
+++ src/script.js
@@ -1,32 +1,5 @@
-// Объект Модального окна
-const modalWindow = {
-    mainDiv: document.getElementById('modal-window'),
-    closeButton: document.getElementById('modal-close-button'),
-    saveButton: document.getElementById('modal-save-button'),
-    isOpen: function() {
-        return this.mainDiv.style.display === 'flex';
-    },
-    showWindow: function() {
-        this.mainDiv.style.display = 'flex';
-    },
-    closeWindow: function() {
-        this.mainDiv.style.display = 'none';
-    }
-};
-
-modalWindow.mainDiv.addEventListener('click', (event) => {
-    const isMainDiv = event.target.getAttribute('id') === 'modal-window';
-    if (modalWindow.isOpen() && isMainDiv) {
-        modalWindow.closeWindow();
-    }
-});
-
-modalWindow.closeButton.addEventListener('click', () => modalWindow.closeWindow());
-
-
-
 // Форма модального окна
-const modalForm = document.getElementById('modal-form');
+const modalForm = document.getElementById('modalForm');
 modalForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -34,6 +7,5 @@ modalForm.addEventListener('submit', (event) => {
 });
 
 // Кнопка добавления задачи
-const addTaskButton = document.getElementById('add-task-button');
+const addTaskButton = document.getElementById('addTaskButton');
 addTaskButton.addEventListener('click', () => modalWindow.showWindow());
-
